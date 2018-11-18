187c376e07e0be8be384617d0de0e2cff1f5b5c3
user144166 <user144166@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-11-18 10:32:46 -0500

    Story 3

--- Canvas/New Group/CanvasViewController.swift
+++ Canvas/New Group/CanvasViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CanvasViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,7 +18,30 @@ class CanvasViewController: UIViewController {
     }
     
 
-    /*
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {_ = sender.translation(in: view)    }
+    
+
+    func didPan(sender: UIPanGestureRecognizer) {
+        _ = sender.location(in: view)
+        _ = sender.velocity(in: view)
+        _ = sender.translation(in: view)
+        
+        if sender.state == .began {
+            print("Gesture began")
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+        } else if sender.state == .ended {
+            print("Gesture ended")
+        }
+    }
+    
+    let trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    trayDownOffset = 160
+    trayUp = trayView.center // The initial position of the tray
+    trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
